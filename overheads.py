d24f5b7461d4043a9db56607de79979f6a724760
LAKYidkanymore <S10239920@connect.np.edu.sg>
2022-08-04 21:41:44 +0800

    test

--- overheads.py
+++ overheads.py
@@ -2,3 +2,14 @@ import csv, API
 from pathlib import Path
 
 forex=API.api_function()
+
+def overhead(forex):
+    path=str(Path.cwd())+"\csv_report\overheads.csv"
+
+
+    rows=[]
+    with open(path,"r") as a:
+        csvreader = csv.reader(a)
+        next(csvreader)
+
+        for row in csvreader:rows.append(row)
