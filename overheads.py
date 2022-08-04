ae0d449146ee48f0b20d5c8ee515dbfd7ca83347
LAKYidkanymore <S10239920@connect.np.edu.sg>
2022-08-04 23:57:32 +0800

    adding comments

--- overheads.py
+++ overheads.py
@@ -3,10 +3,14 @@ from pathlib import Path
 
 forex=API.api_function()
 
+
 def overhead(forex):
+    #use of def to define a function
     path=str(Path.cwd())+"\csv_report\overheads.csv"
+    #file name
 
     rows=[]
+    #create empty list for entry of data
     with open(path,"r") as a:
         csvreader = csv.reader(a)
         next(csvreader)
@@ -14,8 +18,10 @@ def overhead(forex):
         for row in csvreader:rows.append(row)
         
     max=0
-
+    #the first number in overheads.csv can be max since all overheads is >0.
+    #After the first number is max, the code will use the max and compare with subsequence data to aquire new max
     Highest_overhead = []
+    #creating a second empty list for entry of data
 
     for i in range(len(rows)):
         if max<float(rows[i][1]):
